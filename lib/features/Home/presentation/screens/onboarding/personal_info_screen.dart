9003b1f4e5a3756a17fd367123749b2c3bef879a
Kartik <kartikkatti55@gmil.com>
2025-08-15 23:31:33 +0530

    Committed

--- lib/features/Home/presentation/screens/onboarding/personal_info_screen.dart
+++ lib/features/Home/presentation/screens/onboarding/personal_info_screen.dart
@@ -1,5 +1,6 @@
 // import 'package:flutter/material.dart';
-// import 'package:intl/intl.dart'; // for formatting date
+// import 'package:flutter_assignment3/core/constants/app_sizes.dart';
+// import 'package:intl/intl.dart';
 // import 'package:flutter_assignment3/core/constants/app_assets.dart';
 // import 'package:flutter_assignment3/core/theme/app_colors.dart';
 // import 'package:flutter_assignment3/core/theme/app_text_styles.dart';
@@ -29,19 +30,22 @@
 //   InputDecoration _inputDecoration({required String hint, Widget? suffixIcon}) {
 //     return InputDecoration(
 //       hintText: hint,
-//       hintStyle: AppTextStyles.titleSmall.copyWith(color: Colors.grey),
+//       hintStyle: AppTextStyles.titleSmall.copyWith(
+//         color: Colors.grey,
+//         fontSize: 14.sp,
+//       ),
 //       filled: true,
 //       fillColor: Colors.white,
 //       suffixIcon: suffixIcon,
-//       contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-//       border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+//       contentPadding: 16.symmetricPadding(horizontal: 16, vertical: 16),
+//       border: OutlineInputBorder(borderRadius: BorderRadius.circular(12.rw)),
 //       enabledBorder: OutlineInputBorder(
-//         borderRadius: BorderRadius.circular(12),
-//         borderSide: BorderSide(color: AppColors.gray400),
+//         borderRadius: BorderRadius.circular(12.rw),
+//         borderSide: BorderSide(color: AppColors.gray400, width: 1.rw),
 //       ),
 //       focusedBorder: OutlineInputBorder(
-//         borderRadius: BorderRadius.circular(12),
-//         borderSide: const BorderSide(color: AppColors.gray400, width: 1.5),
+//         borderRadius: BorderRadius.circular(12.rw),
+//         borderSide: BorderSide(color: AppColors.gray400, width: 1.5.rw),
 //       ),
 //     );
 //   }
@@ -57,20 +61,17 @@
 //           data: Theme.of(context).copyWith(
 //             dialogBackgroundColor: Colors.white,
 //             colorScheme: ColorScheme.light(
-//               primary: AppColors.primary, // Header background color
-//               onPrimary: Colors.white, // Header text color
-//               onSurface: Colors.black, // Body text color
+//               primary: AppColors.primary,
+//               onPrimary: Colors.white,
+//               onSurface: Colors.black,
 //             ),
 //             textButtonTheme: TextButtonThemeData(
-//               style: TextButton.styleFrom(
-//                 foregroundColor: AppColors.primary, // Button text color
-//               ),
+//               style: TextButton.styleFrom(foregroundColor: AppColors.primary),
 //             ),
 //           ),
 //           child: Center(
-//             // makes it smaller
 //             child: ConstrainedBox(
-//               constraints: const BoxConstraints(maxWidth: 350, maxHeight: 500),
+//               constraints: BoxConstraints(maxWidth: 350.rw, maxHeight: 500.rh),
 //               child: child!,
 //             ),
 //           ),
@@ -97,68 +98,74 @@
 //         totalPages: widget.totalPages,
 //         onBack: widget.onBack,
 //       ),
+//       resizeToAvoidBottomInset: false, // important
 //       body: Padding(
-//         padding: const EdgeInsets.all(15.0),
+//         padding: 16.allPadding,
 //         child: Column(
 //           crossAxisAlignment: CrossAxisAlignment.start,
 //           children: [
-//             SingleChildScrollView(
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   _labelWithAsterisk("First Name"),
-//                   const SizedBox(height: 8),
-//                   TextField(
-//                     cursorColor: Colors.grey,
-//                     decoration: _inputDecoration(hint: 'Enter your first name'),
-//                   ),
-//                   const SizedBox(height: 20),
-
-//                   _labelWithAsterisk("Last Name"),
-//                   const SizedBox(height: 8),
-//                   TextField(
-//                     cursorColor: Colors.grey,
-//                     decoration: _inputDecoration(hint: 'Enter your last name'),
-//                   ),
-//                   const SizedBox(height: 20),
-
-//                   _labelWithAsterisk("Gender"),
-//                   const SizedBox(height: 8),
-//                   Row(
-//                     children: [
-//                       _genderOption(AppAssets.femaleIcon, "Male"),
-//                       _genderOption(AppAssets.maleIcon, "Female"),
-//                       _genderOption(AppAssets.otherIcon, "Other"),
-//                     ],
-//                   ),
-//                   const SizedBox(height: 20),
-
-//                   _labelWithAsterisk("Date of Birth"),
-//                   const SizedBox(height: 8),
-//                   TextField(
-//                     cursorColor: Colors.grey,
-//                     readOnly: true,
-//                     controller: TextEditingController(
-//                       text: _selectedDate != null
-//                           ? DateFormat('dd/MM/yyyy').format(_selectedDate!)
-//                           : '',
-//                     ),
-//                     decoration: _inputDecoration(
-//                       hint: 'Select your date of birth',
-//                       suffixIcon: const Icon(
-//                         Icons.calendar_today,
-//                         color: Colors.grey,
+//             Expanded(
+//               child: SingleChildScrollView(
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     _labelWithAsterisk("First Name"),
+//                     8.vSpace,
+//                     TextField(
+//                       cursorColor: Colors.grey,
+//                       decoration: _inputDecoration(
+//                         hint: 'Enter your first name',
 //                       ),
 //                     ),
-//                     onTap: _pickDateOfBirth,
-//                   ),
-//                 ],
+//                     20.vSpace,
+
+//                     _labelWithAsterisk("Last Name"),
+//                     8.vSpace,
+//                     TextField(
+//                       cursorColor: Colors.grey,
+//                       decoration: _inputDecoration(
+//                         hint: 'Enter your last name',
+//                       ),
+//                     ),
+//                     20.vSpace,
+
+//                     _labelWithAsterisk("Gender"),
+//                     8.vSpace,
+//                     Row(
+//                       children: [
+//                         _genderOption(AppAssets.maleIcon, "Male"),
+//                         _genderOption(AppAssets.femaleIcon, "Female"),
+//                         _genderOption(AppAssets.otherIcon, "Other"),
+//                       ],
+//                     ),
+//                     20.vSpace,
+
+//                     _labelWithAsterisk("Date of Birth"),
+//                     8.vSpace,
+//                     TextField(
+//                       cursorColor: Colors.grey,
+//                       readOnly: true,
+//                       controller: TextEditingController(
+//                         text: _selectedDate != null
+//                             ? DateFormat('dd/MM/yyyy').format(_selectedDate!)
+//                             : '',
+//                       ),
+//                       decoration: _inputDecoration(
+//                         hint: 'Select your date of birth',
+//                         suffixIcon: Icon(
+//                           Icons.calendar_today,
+//                           color: Colors.grey,
+//                           size: 20.rw,
+//                         ),
+//                       ),
+//                       onTap: _pickDateOfBirth,
+//                     ),
+//                   ],
+//                 ),
 //               ),
 //             ),
-//             // ),
-//             Spacer(),
 //             CustomButton(text: "Let’s Begin", onPressed: () {}),
-//             const SizedBox(height: 30),
+//             30.vSpace,
 //           ],
 //         ),
 //       ),
@@ -171,11 +178,17 @@
 //         children: [
 //           TextSpan(
 //             text: label,
-//             style: AppTextStyles.titleSmall.copyWith(color: AppColors.gray700),
+//             style: AppTextStyles.titleSmall.copyWith(
+//               color: AppColors.gray700,
+//               fontSize: 14.sp,
+//             ),
 //           ),
 //           TextSpan(
 //             text: " *",
-//             style: AppTextStyles.titleSmall.copyWith(color: AppColors.error),
+//             style: AppTextStyles.titleSmall.copyWith(
+//               color: AppColors.error,
+//               fontSize: 14.sp,
+//             ),
 //           ),
 //         ],
 //       ),
@@ -192,24 +205,25 @@
 //           });
 //         },
 //         child: Container(
-//           margin: const EdgeInsets.symmetric(horizontal: 4),
-//           padding: const EdgeInsets.symmetric(vertical: 12),
+//           margin: 4.symmetricPadding(horizontal: 4),
+//           padding: 12.symmetricPadding(vertical: 12),
 //           decoration: BoxDecoration(
 //             color: isSelected ? AppColors.accentBlue : Colors.white,
-//             borderRadius: BorderRadius.circular(12),
+//             borderRadius: BorderRadius.circular(12.rw),
 //             border: Border.all(
 //               color: isSelected ? AppColors.secondary : Colors.grey.shade300,
-//               width: 1.5,
+//               width: 1.5.rw,
 //             ),
 //           ),
 //           child: Column(
 //             children: [
-//               Image.asset(iconPath, height: 24, width: 24),
-//               const SizedBox(height: 6),
+//               Image.asset(iconPath, height: 24.rh, width: 24.rw),
+//               6.vSpace,
 //               Text(
 //                 label,
 //                 style: AppTextStyles.titleSmall.copyWith(
 //                   color: AppColors.gray700,
+//                   fontSize: 14.sp,
 //                 ),
 //               ),
 //             ],
@@ -312,6 +326,7 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.white,
+      resizeToAvoidBottomInset: true, // important for keyboard
       appBar: IntroAppBar(
         title: "Hey there! Let's get to know you.",
         subtitle:
@@ -320,12 +335,12 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
         totalPages: widget.totalPages,
         onBack: widget.onBack,
       ),
-      resizeToAvoidBottomInset: false, // important
       body: Padding(
         padding: 16.allPadding,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // Scrollable form
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
@@ -382,11 +397,15 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
                       ),
                       onTap: _pickDateOfBirth,
                     ),
+                    20.vSpace,
+                    CustomButton(text: "Let’s Begin", onPressed: () {}),
                   ],
                 ),
               ),
             ),
-            CustomButton(text: "Let’s Begin", onPressed: () {}),
+
+            // Fixed button at bottom
+            // CustomButton(text: "Let’s Begin", onPressed: () {}),
             30.vSpace,
           ],
         ),
